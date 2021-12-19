ec9ad28828d9986c904476ed9e249c98adb76d18
Radoslav Yordanov <yordanov1234@gmail.com>
2021-12-19 14:50:12 +0200

    Added implementation for retriving photos at DashboardViewModel

--- app/src/main/java/com/example/photo_gallery/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/photo_gallery/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,27 @@
 package com.example.photo_gallery.ui.dashboard;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.photo_gallery.data.models.photo.PhotoImpl;
+import com.example.photo_gallery.data.repositories.photo.PhotoRepository;
+import com.example.photo_gallery.data.repositories.photo.PhotoRepositoryImpl;
+
+import java.util.List;
 
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private PhotoRepository photoRepository;
 
     public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        this.photoRepository = new PhotoRepositoryImpl();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<PhotoImpl>> getAllPhotos () {
+        return this.photoRepository.getAll();
+    }
+
+    public  LiveData<PhotoImpl> getPhotoById (String id) {
+        return  this.photoRepository.getPhotoById(id);
     }
 }
