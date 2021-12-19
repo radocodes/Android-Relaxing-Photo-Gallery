4ebe615d35a57a9fa2a2322eaa1a87085f288a4e
Radoslav Yordanov <yordanov1234@gmail.com>
2021-12-19 10:47:17 +0200

    Added Photo POJO interface and implementation

---
+++ app/src/main/java/com/example/photo_gallery/data/contracts/Photo.java
@@ -0,0 +1,16 @@
+package com.example.photo_gallery.data.contracts;
+
+public interface Photo {
+
+    public String getId();
+
+    public String getAuthor();
+
+    public int getWidth();
+
+    public int getWeight();
+
+    public String getUrl();
+
+    public String getDownload_url();
+}
---
+++ app/src/main/java/com/example/photo_gallery/data/implementation/PhotoImpl.java
@@ -0,0 +1,44 @@
+package com.example.photo_gallery.data.implementation;
+
+public class PhotoImpl {
+    private final String id;
+    private final String author;
+    private final int width;
+    private final int weight;
+    private final String url;
+    private final String download_url;
+
+    public PhotoImpl (String id, String author, int width, int weight, String url, String download_url)
+    {
+        this.id = id;
+        this.author = author;
+        this.width = width;
+        this.weight = weight;
+        this.url = url;
+        this.download_url = download_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDownload_url() {
+        return download_url;
+    }
+}
