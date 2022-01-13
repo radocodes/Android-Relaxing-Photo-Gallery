a195ec09129255755773fb9a96618f3e4cea7852
Radoslav Yordanov <yordanov1234@gmail.com>
2022-01-13 10:18:09 +0200

    Moved getPhotoWebApiClient @Provides method for Retrofit from PhotoModule to NetworkModule

---
+++ app/src/main/java/com/example/photo_gallery/di/NetworkModule.java
@@ -0,0 +1,27 @@
+package com.example.photo_gallery.di;
+
+import com.example.photo_gallery.data.network.PhotoWebApiClient;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class NetworkModule {
+
+    public static final String PHOTO_WEB_API_BASE_URL = "https://picsum.photos";
+
+    @Provides
+    public static PhotoWebApiClient getPhotoWebApiClient() {
+        return new Retrofit.Builder()
+                .baseUrl(PHOTO_WEB_API_BASE_URL)
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(PhotoWebApiClient.class);
+    }
+}
--- app/src/main/java/com/example/photo_gallery/di/PhotoModule.java
+++ app/src/main/java/com/example/photo_gallery/di/PhotoModule.java
@@ -1,6 +1,5 @@
 package com.example.photo_gallery.di;
 
-import com.example.photo_gallery.data.network.PhotoWebApiClient;
 import com.example.photo_gallery.data.repositories.photo.PhotoRepository;
 import com.example.photo_gallery.data.repositories.photo.PhotoRepositoryImpl;
 
@@ -8,24 +7,10 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 @InstallIn(SingletonComponent.class)
 public class PhotoModule {
-
-    public static final String PHOTO_WEB_API_BASE_URL = "https://picsum.photos";
-
-    @Provides
-    public static PhotoWebApiClient getPhotoWebApiClient() {
-        return new Retrofit.Builder()
-                .baseUrl(PHOTO_WEB_API_BASE_URL)
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build().create(PhotoWebApiClient.class);
-    }
 
     @Provides
     public static PhotoRepository getPhotoRepository (PhotoRepositoryImpl photoRepositoryImpl) {
