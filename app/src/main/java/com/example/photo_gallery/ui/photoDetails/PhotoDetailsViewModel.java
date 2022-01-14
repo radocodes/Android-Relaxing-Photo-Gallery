bdcc36641770820edd6538af298691cffcff05f9
Radoslav Yordanov <yordanov1234@gmail.com>
2022-01-14 11:22:37 +0200

    PhotoDetailsFragment - Implemented bookmarking/favorites handling and improved fetchPhotoDetailsByPhotoId async call

--- app/src/main/java/com/example/photo_gallery/ui/photoDetails/PhotoDetailsViewModel.java
+++ app/src/main/java/com/example/photo_gallery/ui/photoDetails/PhotoDetailsViewModel.java
@@ -1,13 +1,83 @@
 package com.example.photo_gallery.ui.photoDetails;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.photo_gallery.data.models.photo.FavoritePhotoId;
 import com.example.photo_gallery.data.models.photo.PhotoData;
+import com.example.photo_gallery.data.models.photo.PhotoDataImpl;
+import com.example.photo_gallery.data.repositories.photo.PhotoRepository;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+@HiltViewModel
 public class PhotoDetailsViewModel extends ViewModel {
 
     private final String SINGLE_WHITE_SPACE = " ";
     private final String NEW_ROW = "\n";
+
+    private PhotoRepository photoRepository;
+
+    private MutableLiveData<PhotoDataImpl> photoData;
+    private MutableLiveData<Boolean> isBookmarked;
+
+    @Inject
+    public  PhotoDetailsViewModel(PhotoRepository photoRepository) {
+        this.photoRepository = photoRepository;
+        this.photoData = new MutableLiveData<>();
+        this.isBookmarked = new MutableLiveData<>();
+    }
+
+    public void fetchPhotoDetailsByPhotoId(String id) {
+        this.photoRepository.getPhotoById(id)
+                .subscribeOn(Schedulers.io())
+                .subscribe(photoData -> {
+                    photoData.setId(id);
+                    this.photoData.postValue(photoData);
+        }, throwable -> {throwable.printStackTrace();});
+    }
+
+    public LiveData<PhotoDataImpl> getPhotoDetails() {
+        return this.photoData;
+    }
+
+    public LiveData<Boolean> isBookmarked() {
+        return this.isBookmarked;
+    }
+
+    public void fetchIsBookmarked(String externalId) {
+        photoRepository.getBookmarkedByExternalId(externalId)
+                .subscribeOn(Schedulers.io())
+                .onErrorReturn(defaultValue -> new FavoritePhotoId())
+                .subscribe(bookmarkedPhotoIdHolder -> {
+                    if (bookmarkedPhotoIdHolder.getId() == 0) {
+                        this.isBookmarked.postValue(false);
+                    }
+                    else {
+                        this.isBookmarked.postValue(true);
+                    }
+        }, throwable -> {throwable.printStackTrace();});
+    }
+
+    public void addToFavorites(PhotoDataImpl photoData) {
+        this.photoRepository.addToFavorites(new FavoritePhotoId(photoData.getId()))
+                .subscribeOn(Schedulers.io())
+                .subscribe(() -> {
+                    this.isBookmarked.postValue(true);
+        });
+    }
+
+    public void removeFromFavorites(String externalId) {
+        this.photoRepository.removeFromFavorites(externalId)
+                .subscribeOn(Schedulers.io())
+                .subscribe(() -> {
+                    this.isBookmarked.postValue(false);
+                });
+    }
 
     public String PrepareTextDetailsForPreview(
             String authorNameTicket,
