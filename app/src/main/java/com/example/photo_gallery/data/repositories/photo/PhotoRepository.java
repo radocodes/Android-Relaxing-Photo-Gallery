7d32b207c00624c3fbd4bbd6afc703efc51aa3f0
Radoslav Yordanov <yordanov1234@gmail.com>
2022-01-14 11:04:55 +0200

    Added favorites/bookmark flow at PhotoRepository and PhotoRepositoryImpl

--- app/src/main/java/com/example/photo_gallery/data/repositories/photo/PhotoRepository.java
+++ app/src/main/java/com/example/photo_gallery/data/repositories/photo/PhotoRepository.java
@@ -1,14 +1,24 @@
 package com.example.photo_gallery.data.repositories.photo;
 
+import com.example.photo_gallery.data.models.photo.FavoritePhotoId;
 import com.example.photo_gallery.data.models.photo.PhotoDataImpl;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
 
 public interface PhotoRepository{
 
-    public Single<List<PhotoDataImpl>> getAllPhotos();
+    Single<List<PhotoDataImpl>> getAllPhotos();
 
-    public Single<PhotoDataImpl> getPhotoById (String id);
+    Single<PhotoDataImpl> getPhotoById (String id);
+
+    Single<List<FavoritePhotoId>> getAllFavoriteIds();
+
+    Single<FavoritePhotoId> getBookmarkedByExternalId(String externalId);
+
+    Completable addToFavorites(FavoritePhotoId favoritePhotoId);
+
+    Completable removeFromFavorites(String externalId);
 }
--- app/src/main/java/com/example/photo_gallery/data/repositories/photo/PhotoRepositoryImpl.java
+++ app/src/main/java/com/example/photo_gallery/data/repositories/photo/PhotoRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.photo_gallery.data.repositories.photo;
 
+import com.example.photo_gallery.data.db.FavoritePhotoIdsDao;
+import com.example.photo_gallery.data.models.photo.FavoritePhotoId;
 import com.example.photo_gallery.data.models.photo.PhotoDataImpl;
 import com.example.photo_gallery.data.network.PhotoWebApiClient;
 
@@ -7,15 +9,18 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
 
 public class PhotoRepositoryImpl implements PhotoRepository {
 
     private PhotoWebApiClient photoWebApiClient;
+    private FavoritePhotoIdsDao favoritePhotoIdsDao;
 
     @Inject
-    public PhotoRepositoryImpl(PhotoWebApiClient photoWebApiClient) {
+    public PhotoRepositoryImpl(PhotoWebApiClient photoWebApiClient, FavoritePhotoIdsDao favoritePhotoIdsDao) {
         this.photoWebApiClient = photoWebApiClient;
+        this.favoritePhotoIdsDao = favoritePhotoIdsDao;
     }
 
     @Override
@@ -25,6 +30,26 @@ public class PhotoRepositoryImpl implements PhotoRepository {
 
     @Override
     public Single<PhotoDataImpl> getPhotoById(String id) {
-        return photoWebApiClient.getPhotoById(id);
+        return this.photoWebApiClient.getPhotoById(id);
+    }
+
+    @Override
+    public Single<List<FavoritePhotoId>> getAllFavoriteIds() {
+        return this.favoritePhotoIdsDao.getAll();
+    }
+
+    @Override
+    public Single<FavoritePhotoId> getBookmarkedByExternalId(String externalId) {
+        return this.favoritePhotoIdsDao.getByExternalId(externalId);
+    }
+
+    @Override
+    public Completable addToFavorites(FavoritePhotoId favoritePhotoId) {
+        return this.favoritePhotoIdsDao.insert(favoritePhotoId);
+    }
+
+    @Override
+    public Completable removeFromFavorites(String externalId) {
+        return this.favoritePhotoIdsDao.delete(externalId);
     }
 }
